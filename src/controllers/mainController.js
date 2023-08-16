4a339c7d6da6496788b41b0e6617c694394c3fe6
Luciano <luciano.carrizo26@gmail.com>
2023-08-16 15:30:12 -0300

    parte 1 CRUD

---
+++ src/controllers/mainController.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		const productVisited = products.filter(product => product.category == "visited")
+		const productInSale = products.filter(product => product.category == "in-sale")
+		return res.render('index', {
+			productVisited,
+			productInSale,
+		})
+	},
+	search: (req, res) => {
+		const results = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))
+		return res.render('results', {
+			results,
+			toThousand,
+			keywords : req.query.keywords
+		})
+	},
+};
+
+module.exports = controller;
